97310a983016ea39c6ac590c10785ff0e81b8ed7
Ken VanDine <ken@vandine.org>
2024-11-26 16:17:01 -0500

    Added tray icon

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Tray, Menu, nativeImage } = require('electron');
+
+let tray = null;
+let win = null;
 
 function createWindow () {
-  const win = new BrowserWindow({
+  const icon = nativeImage.createFromPath('/snap/ken-copilot/current/icon.png');
+  win = new BrowserWindow({
     width: 800,
-    height: 600,
+    height: 800,
+    icon: icon,
     webPreferences: {
       nodeIntegration: true
     }
@@ -11,17 +16,45 @@ function createWindow () {
 
   win.removeMenu();
   win.loadURL('https://copilot.microsoft.com');
+
+  tray = new Tray(icon);
+  // Ignore double click events for the tray icon
+  tray.setIgnoreDoubleClickEvents(true)
+
+  const contextMenu = Menu.buildFromTemplate([
+	  { label: 'Copilot', click: () => focusWindow()},
+	  { label: 'Quit', click: () => app.quit() }
+  ]);
+  tray.setTitle('Copilot');
+  tray.setToolTip('Copilot');
+  tray.setContextMenu(contextMenu);
+  tray.on('click', () => {
+	  console.log("click");
+	  focusWindow();
+  });
+}
+
+function focusWindow() {
+  console.log("focusWindow");
+  if (win.isFocused()) {
+    win.hide();
+  } else {
+    win.show();
+  }
 }
 
 app.whenReady().then(createWindow);
 
+/*
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
+*/
 
 app.on('activate', () => {
+	console.log("ACTIVATE");
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
